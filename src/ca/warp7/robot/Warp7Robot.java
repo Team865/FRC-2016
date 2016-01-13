31b417eb4e78f80538f8f8dd2068bfbb5082e993
Ricardo <homiericky@gmail.com>
2016-01-13 14:34:40 -0500

    Initial commit

---
+++ src/ca/warp7/robot/Warp7Robot.java
@@ -0,0 +1,41 @@
+package ca.warp7.robot;
+
+import edu.wpi.first.wpilibj.*;
+
+public class Warp7Robot extends SampleRobot {
+	
+	public Warp7Robot() {
+		
+	}
+	
+	public void robotInit() {
+		
+	}
+	
+	public void disablesd() {
+		
+		while(isDisabled()) {
+			
+		}
+	}
+	
+	public void autonomous() {
+		
+		while(isAutonomous()) {
+			
+		}
+	}
+	
+	public void operatorControl() {
+		
+		while(isOperatorControl()) {
+			
+		}
+	}
+	
+	public void test() {
+		
+	}
+		
+	
+}
