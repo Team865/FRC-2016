a19365d6791b54b56bf52bb3c6ab295ecf23e7a7
David Petkovsek <wabatheboba@gmail.com>
2016-10-22 08:53:12 -0400

    added donothing auto

---
+++ src/ca/warp7/robot/autonomous/DoNothin.java
@@ -0,0 +1,18 @@
+package ca.warp7.robot.autonomous;
+
+import ca.warp7.robot.subsystems.Drive;
+import ca.warp7.robot.subsystems.Intake;
+import ca.warp7.robot.subsystems.Shooter;
+
+public class DoNothin extends AutonomousBase{
+
+	public DoNothin(Drive drive, Shooter shooter, Intake intake) {
+		// nothing
+	}
+
+	@Override
+	public void periodic(Drive drive, Shooter shooter, Intake intake) {
+		// nothing
+	}
+
+}
